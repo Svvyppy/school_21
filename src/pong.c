30f3598bb2ac040aa253263276eb3ad6a0cc088b
swyppy <nikitabulatov33@gmail.com>
2024-07-21 02:40:37 +0300

    beta version

--- src/pong.c
+++ src/pong.c
@@ -9,8 +9,9 @@
 void ball_move();  // 1 - down_left, 2 - left, 3 - up_left, 4 - up_right, 5 -
                    // right, 6 - down_right
 void ball_change_dir();
-void rocket_move(int);
+void rocket_move();
 int score();
+void restart();
 
 struct ball {
     int x;
@@ -24,10 +25,21 @@ struct rocket {
     int right_y;
     int right_x;
 
-
 } rocket;
 
-int main() {}
+int main() {
+    int flag;  //флаг, который передается в функцию Андрея для контроля счета
+
+    //тут должны быть функии отрисовки поля
+
+    rocket_move();
+    ball_move();
+    ball_change_dir();
+    flag = score();
+    if (getchar == 'r') {
+        restart();
+    }
+}
 
 //функия описывает движение шарика в зависимости от направления
 void ball_move() {
@@ -49,8 +61,8 @@ void ball_move() {
             ball.y++;
     }
 }
-/*функция для смены направления движения шарика при столкновении со стеной или ракеткой, 
-при движении влево или вправо при столкновении выбирается рандомное направление, чтобы 
+/*функция для смены направления движения шарика при столкновении со стеной или ракеткой,
+при движении влево или вправо при столкновении выбирается рандомное направление, чтобы
 шарик не летал просто по прямой
  */
 void ball_change_dir() {
@@ -97,18 +109,23 @@ void ball_change_dir() {
         }
     }
 }
-/*функция принимает значение, которое вводит user_1/user_2 и перемещает ракетку 
+/*функция принимает значение, которое вводит user_1/user_2 и перемещает ракетку
   на одну единицу вверх или вниз в диапазоне {1,17}.
 */
-void rocket_move(int a) {
-    switch (a) {
+void rocket_move() {
+    int user_1;
+    int user_2;
+    user_1 = getchar();
+    switch (user_1) {
         case 'a':
             rocket.left_y++;
             break;
         case 'z':
             rocket.left_y--;
             break;
-
+    }
+    user_2 = getchar();
+    switch (user_2) {
         case 'k':
             rocket.right_y++;
             break;
@@ -119,12 +136,19 @@ void rocket_move(int a) {
 }
 
 int score() {
-  if (ball.x == 0) {
-    return 0;
-  }
-  if (ball.x == 21)
-  {
-    return 1;
-  }
-  return 2;
+    if (ball.x > rocket.left_x) {
+        ball.x = 39;
+        ball.y = 10;
+        ball.dir = 2;
+        return 0;
+    }
+    if (ball.x == 21) {
+        ball.x = 39;
+        ball.y = 10;
+        ball.dir = 5;
+        return 1;
+    }
+    return 2;
 }
+
+void restart() { /*нужно обнулить счет и очистить поле*/ }
