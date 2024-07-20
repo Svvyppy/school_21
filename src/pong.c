11d69033c735ee6ce6bed039761dfde3be8ab924
swyppy <nikitabulatov33@gmail.com>
2024-07-21 02:11:57 +0300

    add commit and edited ball_move

--- src/pong.c
+++ src/pong.c
@@ -9,9 +9,8 @@
 void ball_move();  // 1 - down_left, 2 - left, 3 - up_left, 4 - up_right, 5 -
                    // right, 6 - down_right
 void ball_change_dir();
-void rocket_right(int);
-void rocket_left(int);
-int score(int);
+void rocket_move(int);
+int score();
 
 struct ball {
     int x;
@@ -21,12 +20,16 @@ struct ball {
 
 struct rocket {
     int left_y;
+    int left_x;
     int right_y;
+    int right_x;
+
 
 } rocket;
 
 int main() {}
 
+//функия описывает движение шарика в зависимости от направления
 void ball_move() {
     switch (ball.dir) {
         case 4:
@@ -46,9 +49,12 @@ void ball_move() {
             ball.y++;
     }
 }
-
+/*функция для смены направления движения шарика при столкновении со стеной или ракеткой, 
+при движении влево или вправо при столкновении выбирается рандомное направление, чтобы 
+шарик не летал просто по прямой
+ */
 void ball_change_dir() {
-    if (ball.x == rocket.left_y) {
+    if ((ball.x == rocket.left_x) && ((ball.y <= rocket.left_y) && (ball.y >= rocket.left_y - 3))) {
         switch (ball.dir) {
             case 1:
                 ball.dir = 6;
@@ -61,7 +67,7 @@ void ball_change_dir() {
                 break;
         }
     }
-    if (ball.x == rocket_right) {
+    if ((ball.x == rocket.left_x) && ((ball.y <= rocket.left_y) && (ball.y >= rocket.left_y - 3))) {
         switch (ball.dir) {
             case 6:
                 ball.dir = 1;
@@ -91,8 +97,10 @@ void ball_change_dir() {
         }
     }
 }
-
-void rocket_left(int a) {
+/*функция принимает значение, которое вводит user_1/user_2 и перемещает ракетку 
+  на одну единицу вверх или вниз в диапазоне {1,17}.
+*/
+void rocket_move(int a) {
     switch (a) {
         case 'a':
             rocket.left_y++;
@@ -108,4 +116,15 @@ void rocket_left(int a) {
             rocket.right_y--;
             break;
     }
+}
+
+int score() {
+  if (ball.x == 0) {
+    return 0;
+  }
+  if (ball.x == 21)
+  {
+    return 1;
+  }
+  return 2;
 }
